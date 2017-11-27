ad4c0687c4c9fb2a926247cbb7f5947cee703928
Ryan <33066159+SimmsRyan97@users.noreply.github.com>
2017-11-27 00:06:46 +0000

    Update slidingTile.js

--- code/production/scripts/slidingTile.js
+++ code/production/scripts/slidingTile.js
@@ -1,6 +1,7 @@
 document.addEventListener ("DOMContentLoaded", handleDocumentLoad);
 
-function handleDocumentLoad() {
+function handleDocumentLoad()
+{
 	
 	/* Hides / Unhides Canvas until start */
 	var start = document.getElementById('start');
@@ -138,7 +139,7 @@ function handleDocumentLoad() {
 	image.addEventListener('load', createTiles);
 	var canvSize = 600; // pixels
 	canvas.width = canvSize;
-	canvas.height = canvSize;    
+	canvas.height = canvSize;
     
 	var cells = 3;
     var cellSize = canvSize/cells;
@@ -151,102 +152,130 @@ function handleDocumentLoad() {
     var emptySquare = new Tile(cells-1, cells-1, null);
     
     createEmptyBoard();
-    function createEmptyBoard() {
-        	board = [];
-        	board.length = rows;
-        	for(var i = 0; i < rows; i++) {
-           	 var row = [];
-            	for( var j = 0; j < columns; j++) {
-                	row.push(null);
-            	}
-            	board[i] = row;
-        	}
-    	}
 	
-    	function onMouseUpdate(e)
+    function createEmptyBoard()
+	{
+		board = [];
+		board.length = rows;
+		for(var i = 0; i < rows; i++)
 		{
-			var rect = canvas.getBoundingClientRect();
-			mouseX = e.pageX - rect.left;
-			mouseY = e.pageY - rect.top;
+			var row = [];
+			for( var j = 0; j < columns; j++)
+			{
+				row.push(null);
+			}
+				board[i] = row;
 		}
+	}
 	
-    	function checkMouse() {
-        	for(var x = 0; x < columns; x++) {
-           	for(var y = 0; y < rows; y++) {
-                	if(y == columns-1 && x == rows-1) {
-                   }
-               else {
-                   if(mouseX > board[x][y].x) {
-                       if(mouseX < board[x][y].x + cellSize) {
-                           if(mouseY > board[x][y].y) {
-                               if(mouseY < board[x][y].y + cellSize) {
-                                    checkPosition(board[x][y]);
-                                   }
-                                }
-                           }
-                       }
-                  }
-                   
-               }    
-           }
-       }
+	function onMouseUpdate(e)
+	{
+		var rect = canvas.getBoundingClientRect();
+		mouseX = e.pageX - rect.left;
+		mouseY = e.pageY - rect.top;
+	}
+	
+    function checkMouse()
+	{
+		for(var x = 0; x < columns; x++)
+		{
+			for(var y = 0; y < rows; y++)
+			{
+				if(y == columns-1 && x == rows-1)
+				{
+					
+				}
+				else
+				{
+					if(mouseX > board[x][y].x)
+					{
+						if(mouseX < board[x][y].x + cellSize)
+						{
+							if(mouseY > board[x][y].y)
+							{
+							if(mouseY < board[x][y].y + cellSize)
+								{
+									checkPosition(board[x][y]);
+								}
+                            }
+						}
+					}
+				}
+			}
+		}
+	}
 	   
-    function checkPosition(Tile) {
-        var x = 0;
+    function checkPosition(Tile)
+	{
+		var x = 0;
         var y = 0;
-        if((Tile.x + cellSize == emptySquare.x) && (Tile.y == emptySquare.y)){
+        if((Tile.x + cellSize == emptySquare.x) && (Tile.y == emptySquare.y))
+		{
             emptySquare.x = Tile.x;
             Tile.x += cellSize;
             updateTiles();
         }
-        else if((Tile.x - cellSize == emptySquare.x) && (Tile.y == emptySquare.y)){
+        else if((Tile.x - cellSize == emptySquare.x) && (Tile.y == emptySquare.y))
+		{
             emptySquare.x = Tile.x;
             Tile.x -= cellSize;
             updateTiles();
         }
-        else if((Tile.y + cellSize == emptySquare.y) && (Tile.x == emptySquare.x)){
+        else if((Tile.y + cellSize == emptySquare.y) && (Tile.x == emptySquare.x))
+		{
             emptySquare.y = Tile.y;
             Tile.y += cellSize;
             updateTiles();
         }
-        else if((Tile.y - cellSize == emptySquare.y) && (Tile.x == emptySquare.x)){
+        else if((Tile.y - cellSize == emptySquare.y) && (Tile.x == emptySquare.x))
+		{
             emptySquare.y = Tile.y;
             Tile.y -= cellSize;
             updateTiles();
         }
     }
     
-    function updateTiles(){
-        canvas.width += 0;
-              for(var x = 0; x < columns; x++){
-           for(var y = 0; y < rows; y++){
-                   if(y == columns-1 && x == rows-1){
-                     
-                   }
-               else{
-                    board[y][x].drawTiles();
-               }
-           }
-       }
-    
-}
-    
-    function createTiles(){
-        
-       for(var x = 0; x < columns; x++){
-           for(var y = 0; y < rows; y++){
-               
-                if(y == columns-1 && x == rows-1){}
-               else{
-                   board[y][x] = new Tile(x, y, image);
-                   board[y][x].drawTiles();
-               }
-           }
-       }
+    function updateTiles()
+	{
+		canvas.width += 0;
+		for(var x = 0; x < columns; x++)
+		{
+			for(var y = 0; y < rows; y++)
+			{
+				if(y == columns-1 && x == rows-1)
+				{
+					
+				}
+				else
+				{
+					board[y][x].drawTiles();
+				}
+			}
+		}
     }
+    
+    function createTiles()
+	{
+		for(var x = 0; x < columns; x++)
+		{
+			for(var y = 0; y < rows; y++)
+			{
+				if(y == columns-1 && x == rows-1)
+				{
+					
+				}
+				else
+				{
+					board[y][x] = new Tile(x, y, image);
+					board[y][x].drawTiles();
+				}
+			}
+		}
+	}
 	
-    function Tile(i, j, image){
-        this.i = i;
+    function Tile(i, j, image)
+	{
+		this.i = i;
         this.j = j;
         this.x = i * cellSize;
         this.y = j * cellSize;
@@ -256,11 +285,10 @@ function handleDocumentLoad() {
         this.image = image;
         
     
-        Tile.prototype.drawTiles = function(){
-       
-            ctx.strokeRect(this.x, this.y, this.size, this.size);
+        Tile.prototype.drawTiles = function()
+		{
+			ctx.strokeRect(this.x, this.y, this.size, this.size);
             ctx.drawImage(this.image, this.a , this.b,this.size, this.size, this.x, this.y, this.size, this.size);
         };
-        
     }
-	}
+}
