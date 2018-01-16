edd656392303814017f7fb6744c0456a100a80cb
Connor Waplington <32132360+cconnor12345@users.noreply.github.com>
2018-01-16 13:02:17 +0000

    Update slidingTile.js

--- code/production/scripts/slidingTile.js
+++ code/production/scripts/slidingTile.js
@@ -141,6 +141,12 @@ function handleDocumentLoad()
 	canvas.addEventListener('click', checkMouse);
 	image.addEventListener('load', createTiles);
 	random.addEventListener('click', randomize);
+	window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    	if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      	  e.preventDefault();
+	    }
+	}, false);
 	
     var canvSize = 600; // pixels
     canvas.width = canvSize;
