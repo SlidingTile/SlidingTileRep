8d5ed6701d09c6058bacd7bf122cbb85c7966167
Ryan <33066159+SimmsRyan97@users.noreply.github.com>
2018-03-07 20:28:59 +0000

    Update slidingTile.js

--- code/production/scripts/slidingTile.js
+++ code/production/scripts/slidingTile.js
@@ -7,8 +7,6 @@ function handleDocumentLoad()
 	var medium = document.getElementById('Medium');
 	var hard = document.getElementById('Hard');
 	var instructions = document.getElementById('instructions');
-	var upload = document.getElementById('uploadButton');
-	upload.addEventListener('click', readURL);
 	game.style.visibility = 'hidden';
 	easy.style.display = 'none'; //Hides difficulty button
 	medium.style.display = 'none'; //Hides difficulty button
@@ -99,21 +97,6 @@ function handleDocumentLoad()
 			cars.style.display = 'none';
 			scenery.style.display = 'none';
 			custom.style.display = 'inline';
-		}
-	}
-	
-	/*Allow custom image upload*/
-	function readURL() {
-		if (this.files && this.files[0]) {
-            		var reader = new FileReader();
-			
-			reader.load = function(e) {
-				var custom = document.getElementById('custom');
-                		custom.src = e.target.result;
-                    		custom.style.width = '200px';
-                    		custom.style.height = '200px';
-            		};
-			reader.readAsDataURL(this.files[0]);
 		}
 	}
 	
